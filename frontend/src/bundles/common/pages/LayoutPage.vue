76c2d2efd30cacea6dbee13d0ee82823f8d3bc55
Alexey Tryapko <aatryapko1201@gmail.com>
2022-02-04 13:54:22 +0200

    add session user fetch

--- frontend/src/bundles/common/pages/LayoutPage.vue
+++ frontend/src/bundles/common/pages/LayoutPage.vue
@@ -4,3 +4,21 @@
     <router-view />
   </div>
 </template>
+
+<script>
+  import namespace from '../store/modules/user/namespace';
+  import { mapActions } from 'vuex';
+  import { FETCH_SESSION_USER } from '../store/modules/user/types/actions';
+
+  export default {
+    async created() {
+      await this.fetchUser();
+    },  
+
+    methods: {
+      ...mapActions(namespace, {
+          fetchUser: FETCH_SESSION_USER,
+      }),
+    },
+  };
+</script>
