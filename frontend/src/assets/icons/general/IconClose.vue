f123babee3b779a9cc8a8be7be7f20be1f51ede9
FrontAndBack <vmaklaren@gmail.com>
2022-02-08 23:07:45 +0100

    Add some icons

---
+++ frontend/src/assets/icons/general/IconClose.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="16"
+    height="14"
+    viewBox="0 0 16 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M15 1.87648L13.59 0.583984L8 5.70815L2.41 0.583984L1 1.87648L6.59 7.00065L1 12.1248L2.41 13.4173L8 8.29315L13.59 13.4173L15 12.1248L9.41 7.00065L15 1.87648Z"
+      fill="#51575B"
+      fill-opacity="0.6"
+      stroke="white"
+      stroke-width="0.1" />
+  </svg>
+
+</template>
+
+<script>
+export default {
+
+};
+</script>
