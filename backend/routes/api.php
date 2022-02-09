6ed0d7818a4f5a23c9468cefacc1ba80cab49e3c
FrontAndBack <vmaklaren@gmail.com>
2022-02-09 15:11:23 +0100

    Add v1 for api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'auth'], function () {
+Route::prefix('v1')->group(function () {
+    Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
     Route::get('/me', [AuthController::class, 'me'])->middleware('auth:api');
+    });
 });
