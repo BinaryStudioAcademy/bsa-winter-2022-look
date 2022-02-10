e854bdf7b7ef9b89dc5b16c6f40adf6ec7d0e247
Ruslan <ryska922@gmail.com>
2022-02-10 16:11:41 +0200

    Created "reset-password" template

--- frontend/src/bundles/auth/pages/ResetPasswordPage.vue
+++ frontend/src/bundles/auth/pages/ResetPasswordPage.vue
@@ -1,3 +1,85 @@
 <template>
-  <div>Reset Password Page</div>
+  <validation-observer
+    ref="observer"
+    v-slot="{ invalid }"
+  >
+    <v-container
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="10"
+        md="4"
+        sm="6"
+      >
+        <form @submit.prevent="handleSubmit">
+          <h3>Enter your email to reset password</h3>
+          <validation-provider
+            v-slot="{ errors }"
+            name="email"
+            rules="required|email"
+          >
+            <v-text-field
+              v-model="email"
+              :error-messages="errors"
+              label="E-mail"
+            />
+          </validation-provider>
+
+          <router-link
+            :to="{ name: 'auth-reset_password' }"
+          >
+            Back to login page
+          </router-link>
+          <v-btn class="mr-4" type="submit" :disabled="invalid"> Reset password </v-btn>
+        </form>
+      </v-col>
+    </v-container>
+  </validation-observer>
 </template>
+
+<script>
+import { required, email } from 'vee-validate/dist/rules';
+import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate';
+
+setInteractionMode('eager');
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty',
+});
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid',
+});
+
+export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+
+  props: {
+    invalid: {
+      type: Boolean,
+      default: false,
+    },
+    processing: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data: () => ({
+    email: '',
+  }),
+
+  methods: {
+    handleSubmit() {
+      alert('test');
+    },
+  },
+};
+</script>
