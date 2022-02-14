8ab8b84a2e806a0386ec9e305659b9a253688728
FrontAndBack <vmaklaren@gmail.com>
2022-02-14 21:43:04 +0100

    Add UserSettingRepository

---
+++ frontend/src/bundles/common/repository/contracts/UserSettingRepository.ts
@@ -0,0 +1,20 @@
+import UserSetting from '@/bundles/common/entity/UserSetting';
+import UserSettingRequest from '../requests/UserSettingRequest';
+
+export default interface UserSettingRepository {
+  /**
+   * Create UserSettingRequest
+   *
+   * @param {UserSettingRequest} payload
+   */
+  create(payload: UserSettingRequest): Promise<UserSetting>;
+  /**
+   * Get UserSettingRequest
+   *
+   */
+  get(): Promise<UserSetting>;
+  /**
+   * Update UserSettingRequest
+   */
+  patch(payload: UserSettingRequest): Promise<UserSetting>;
+}
--- frontend/src/bundles/common/repository/requests/ProfileRequest.ts
+++
@@ -1,6 +0,0 @@
-import Profile from '../../entity/Profile';
-
-export default interface ProfileRequest {
-  userId: number,
-  settings: Profile;
-}
