167912431b886e3f4c0c73536ad5d755226b85f1
Roman Mahola <roman.mahola@binary-studio.com>
2022-02-15 17:47:25 +0200

    feat (logs) Implement Sentry
    - delete Seeds Stubs
    - add sentry to the project

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/seed', function () {
-    return Artisan::call('db:seed');
-});
