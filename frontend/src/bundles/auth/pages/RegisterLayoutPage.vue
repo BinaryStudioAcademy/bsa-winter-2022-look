d343fce10df26869e841251c05eb73e818e394c8
idruz <ihor.druz.dev@gmail.com>
2022-02-21 18:12:52 +0200

    Add override style to global

--- frontend/src/bundles/auth/pages/RegisterLayoutPage.vue
+++ frontend/src/bundles/auth/pages/RegisterLayoutPage.vue
@@ -15,9 +15,3 @@
     <router-view />
   </div>
 </template>
-
-<style
-  lang="scss"
->
-@import "@/assets/scss/override.scss";
-</style>
--- frontend/src/bundles/home/components/Header.vue
+++ frontend/src/bundles/home/components/Header.vue
@@ -196,8 +196,6 @@ export default {
   lang="scss"
   scoped
 >
-@import "@/assets/scss/override.scss";
-
 .to-user {
   text-decoration: none;
 }
@@ -208,6 +206,6 @@ export default {
 }
 .to-user .v-list-item__title {
   font-family: 'Lato', sans-serif;
-  font-size: 18px;
+  font-size: 18px !important;
 }
 </style>
