26c75d656ed8d85e87372d2c68defc31268098c3
Ruslan <ryska922@gmail.com>
2022-03-15 21:43:35 +0200

    Fix show validation error messages

--- backend/app/Http/Requests/Api/Auth/ForgotPasswordRequest.php
+++ backend/app/Http/Requests/Api/Auth/ForgotPasswordRequest.php
@@ -11,7 +11,16 @@ class ForgotPasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
+            'email' => 'required|email|exists:users',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email field can not be empty',
+            'email.email' => 'Email field is not valid',
+            'email.exists' => 'User with current email is not found',
         ];
     }
 }
