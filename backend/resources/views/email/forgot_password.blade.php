0ee0f05b6fc02dfa8a11f1a39a98de1bf96b9ee6
Ruslan <ryska922@gmail.com>
2022-03-17 22:00:42 +0200

    Add css for 'reset password' email template

--- backend/resources/views/email/forgot_password.blade.php
+++ backend/resources/views/email/forgot_password.blade.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <title>Reset your password</title>
 </head>
-    <body>
-    <h4>Reset password</h4>
-    We got a request to reset your password.
-    <a href="{{ url(config('app.front_url') . '/auth/change-password/' . $token) }}">Reset password link</a>
-    If you ignore this message, your password will not be changed.
-    </body>
+<body style="background-color: #faf9f9;">
+    <div class="template" style="display:flex; justify-content:center;">
+        <div class="message" style="max-width:340px;">
+            <div class="message-text" style="font-family: 'Lato', serif; font-size: 18px; line-height: 18px; padding: 5px; color: rgba(0, 0, 0, 0.87)">
+                <h3>Reset password</h3>
+                We have received a request for a new password for your account.<br>
+                To create a new password, click on the link below.
+            </div>
+            <a style="display: flex; justify-content: center; align-items: center; color: white; background-color: #fe5faa;
+                      border-radius: 28px; height: 44px; width: 100%; font-size: 18px; text-decoration: none; margin: 10px 0;"
+                href="{{ url(config('app.front_url') . '/auth/change-password/' . $token) }}">Reset password link</a>
+            <div class="message-text" style="font-family: 'Lato', serif; font-size: 18px; line-height: 18px; padding: 5px; color: rgba(0, 0, 0, 0.87)">
+                IF YOU HAVE NOT REQUEST RESETTING PASSWORD, JUST IGNORE THIS MESSAGE.
+            </div>
+        </div>
+    </div>
+</body>
 </html>
