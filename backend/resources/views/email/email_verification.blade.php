cf18a5d207dbb464e3326e2d3121a103619133ed
Ruslan <ryska922@gmail.com>
2022-03-18 12:13:26 +0200

    Fix template text position

--- backend/resources/views/email/email_verification.blade.php
+++ backend/resources/views/email/email_verification.blade.php
@@ -10,6 +10,7 @@
         <div class="message-text" style="display:flex; align-items:center; flex-direction: column; font-family: 'Lato', serif;
                                         font-size: 18px; line-height: 18px; padding: 5px; color: rgba(0, 0, 0, 0.87)">
             <div style="display: flex;"><h4>Hello, friend.</h4></div>
+            <img style="width: 25%; height: auto;" src="https://drive.google.com/uc?export=view&id=1BjC10FhaDyqTbVkMU9eogfggSw2wnaf1" alt="logo-img">
             <p style="text-align: center;">Thanks for registration in Look app. Please verify your email address by clicking the button below.</p>
             <a style="display: flex; justify-content: center; align-items: center; color: white; background-color: #fe5faa;
                       border-radius: 28px; height: 44px; width: 100%; font-size: 18px; text-decoration: none; margin: 10px 0"
