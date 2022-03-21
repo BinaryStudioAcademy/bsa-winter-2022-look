5de4c6d72c675691dcd64815020ac375942cde31
Ruslan <ryska922@gmail.com>
2022-03-21 18:41:47 +0200

    Add fields default values

--- frontend/src/bundles/main/components/UserDetailsForm.vue
+++ frontend/src/bundles/main/components/UserDetailsForm.vue
@@ -36,23 +36,144 @@
           </validation-provider>
           <validation-provider
             v-slot="{ errors }"
-            name = 'Password'
+            name = 'gender_preference'
             rules = 'required|min:3'
           >
-            <v-select
-              :items="genders"
+            <v-autocomplete
+              :items="gender_preferences"
               label="Looking for"
               rounded
               outlined
               :error-messages="errors"
             />
           </validation-provider>
-          <v-select
+          <v-autocomplete
             :items="genders"
             label="Gender"
             rounded
             outlined
           />
+          <validation-provider
+            v-slot="{ errors }"
+            name="location"
+            rules = 'required'
+          >
+            <v-autocomplete
+              v-model="locationSelected"
+              :items="locations"
+              small-chips
+              label="Location"
+              placeholder="Location"
+              rounded
+              outlined
+              clearable
+              :error-messages="errors"
+            />
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Height"
+          >
+            <v-slider
+              v-model="height"
+              min="50"
+              max="250"
+              :thumb-size="25"
+              :error-messages="errors"
+              thumb-label="always"
+              label="Height, sm"
+              inverse-label
+            />
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="Weight"
+          >
+            <v-slider
+              v-model="weight"
+              min="30"
+              max="250"
+              :thumb-size="25"
+              :error-messages="errors"
+              thumb-label="always"
+              label="Weight, kg"
+              inverse-label
+            />
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name="interests"
+            rules = 'required'
+          >
+            <v-autocomplete
+              v-model="interestSelected"
+              :items="interests"
+              small-chips
+              label="Interests"
+              placeholder="Interests"
+              rounded
+              outlined
+              multiple
+              clearable
+              :error-messages="errors"
+            >
+              <template #selection="{ index }">
+                <span
+                  v-if="index === 0"
+                  class="text-overline grey--text text--darken-3 mx-2"
+                >
+                  +{{ interestSelected.length }} Selected
+                </span>
+              </template>
+            </v-autocomplete>
+          </validation-provider>
+
+          <validation-provider
+            v-slot="{ errors }"
+            name="hobbies"
+            rules = 'required'
+          >
+            <v-autocomplete
+              v-model="hobbiesSelected"
+              :items="hobbies"
+              small-chips
+              label="Hobbies"
+              placeholder="Hobbies"
+              rounded
+              outlined
+              multiple
+              clearable
+              :error-messages="errors"
+            >
+              <template #selection="{ index }">
+                <span
+                  v-if="index === 0"
+                  class="text-overline grey--text text--darken-3 mx-2"
+                >
+                  +{{ hobbiesSelected.length }} Selected
+                </span>
+              </template>
+            </v-autocomplete>
+          </validation-provider>
+
+          <validation-provider
+            v-slot="{ errors }"
+            name="about"
+            rules="required|min:10"
+          >
+            <v-textarea
+              v-model="about"
+              clearable
+              clear-icon="mdi-close-circle"
+              rounded
+              label="About yourself"
+              rows="3"
+              hint="At least 10 characters"
+              outlined
+              :error-messages="errors"
+            />
+          </validation-provider>
+
         </div>
       </v-row>
       <v-row class="ma-2">
@@ -60,7 +181,70 @@
           <h3>Social network</h3>
         </div>
         <div class="col-md-5 col-xl-3 col-sm-12">
-          Social parameters
+          <validation-provider
+            v-slot="{ errors }"
+            name = 'email'
+            rules = 'required|min:3|email'
+          >
+            <v-text-field
+              :items="email"
+              label="Email"
+              rounded
+              outlined
+              :error-messages="errors"
+            />
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name = 'instagram'
+          >
+            <v-text-field
+              :items="instagram"
+              label="Instagram"
+              rounded
+              outlined
+              :error-messages="errors"
+            />
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name = 'facebook'
+          >
+            <v-text-field
+              :items="facebook"
+              label="Facebook"
+              rounded
+              outlined
+              :error-messages="errors"
+            />
+          </validation-provider>
+          <validation-provider
+            v-slot="{ errors }"
+            name = 'other_social'
+          >
+            <v-text-field
+              :items="other_social"
+              label="Other"
+              rounded
+              outlined
+              :error-messages="errors"
+            />
+          </validation-provider>
+
+          <v-btn
+            type="submit"
+            :disabled="invalid"
+            class="white--text text-capitalize font-weight-bold d-flex mx-auto mx-md-0"
+            color="primary"
+            large
+            rounded
+            depressed
+            max-width="215"
+            width="100%"
+          >
+            Save changes
+          </v-btn>
+
         </div>
       </v-row>
     </form>
@@ -96,10 +280,51 @@ export default {
     ...mapState(namespace, {
       userInfo: 'user',
     }),
+    hobbies() {
+      return [
+        'Artistic activities such as painting or graphic design',
+        'Community service',
+        'Cooking or baking',
+        'Examples of interests',
+        'Exercising and healthcare',
+        'Outdoor activities',
+        'Playing an instrument',
+        'Team or individual sports',
+        'Travel',
+        'Woodworking or other projects',
+        'Writing or blogging',
+      ];
+    },
+    interests() {
+      return [
+        'Making or listening to music',
+        'Gaming',
+        'Travel',
+        'Art',
+        'Nature',
+        'Social causes',
+        'Foreign languages',
+        'Topical blogs or research',
+        'History',
+        'Theater',
+      ];
+    },
     genders() {
       return [
-        'Male',
-        'Female',
+        'Male', 'Female', 'Other',
+      ];
+    },
+    gender_preferences() {
+      return [
+        'Male', 'Female', 'Both',
+      ];
+    },
+    locations() {
+      return [
+        'Ukraine',
+        'Canada',
+        'USA',
+        'Germany',
       ];
     },
   },
