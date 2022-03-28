82ea560ed88698e63edf79df7fdbf01d3b3c1263
idruz <ihor.druz.dev@gmail.com>
2022-03-28 14:58:30 +0300

    FrequentlyChat component

---
+++ frontend/src/bundles/main/components/FrequentlyChat.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-item-group
+    class="pt-5"
+  >
+    <v-list-item-title
+      class="font-weight-medium h6 mt-3 mb-3"
+    >
+      Frequently chat
+    </v-list-item-title>
+
+    <v-list-item
+      v-for="(item, index) in frequentlyChat"
+      :key="index"
+      class="d-flex pa-0"
+      flat
+    >
+      <v-badge
+        :color="item.online ? 'green' : 'lightGrey'"
+        dot
+        overlap
+        bordered
+        bottom
+        class="mr-3"
+      >
+        <v-avatar
+          size="32"
+        >
+          <img
+            :src="item.image"
+          >
+        </v-avatar>
+      </v-badge>
+
+      <router-link
+        class="d-block w-100"
+        :to="{name: 'main-chat'}"
+      >
+        <v-list-item-title
+          class="d-flex justify-space-between"
+        >
+          <span
+            class="text-14 lightBlack--text"
+          >
+            {{ item.name }}
+          </span>
+          <span
+            v-if="item.message > 0"
+            class="counter-msg primary font-weight-bold text-14 white--text"
+          >
+            {{ item.message }}
+          </span>
+        </v-list-item-title>
+      </router-link>
+    </v-list-item>
+  </v-item-group>
+</template>
+
+<script>
+export default {
+  props: {
+    frequentlyChat: {
+      type: Object,
+      default: undefined,
+    },
+  },
+};
+</script>
--- frontend/src/bundles/main/components/NavbarComponent.vue
+++ frontend/src/bundles/main/components/NavbarComponent.vue
@@ -97,59 +97,9 @@
         <v-divider />
       </v-item-group>
 
-      <v-item-group
-        class="pt-5"
-      >
-        <v-list-item-title
-          class="font-weight-medium h6 mt-3 mb-3"
-        >
-          Frequently chat
-        </v-list-item-title>
-
-        <v-list-item
-          v-for="(item, index) in frequentlyChat"
-          :key="index"
-          class="d-flex pa-0"
-          flat
-        >
-          <v-badge
-            :color="item.online ? 'green' : 'lightGrey'"
-            dot
-            overlap
-            bordered
-            bottom
-            class="mr-3"
-          >
-            <v-avatar
-              size="32"
-            >
-              <img
-                :src="item.image"
-              >
-            </v-avatar>
-          </v-badge>
-
-          <router-link
-            class="d-block w-100"
-            :to="{name: 'main-chat'}"
-          >
-            <v-list-item-title
-              class="d-flex justify-space-between"
-            >
-              <span
-                class="text-14 lightBlack--text"
-              >
-                {{ item.name }}
-              </span>
-              <span
-                class="counter-msg primary font-weight-bold text-14 white--text"
-              >
-                5
-              </span>
-            </v-list-item-title>
-          </router-link>
-        </v-list-item>
-      </v-item-group>
+      <FrequentlyChat
+        :frequently-chat="frequentlyChat"
+      />
     </v-navigation-drawer>
     <v-app-bar
       flat
@@ -215,9 +165,11 @@ import MapIcon from '@/bundles/main/components/icons/MapIcon';
 import ChatIcon from '@/bundles/main/components/icons/ChatIcon';
 import EventsIcon from '@/bundles/main/components/icons/EventsIcon';
 import HasMassageDotIcon from '@/bundles/main/components/icons/HasMassageDotIcon';
+import FrequentlyChat from '@/bundles/main/components/FrequentlyChat';
 
 export default {
   components: {
+    FrequentlyChat,
     LogoIcon,
     ArrowIcon,
     ListIcon,
@@ -262,21 +214,21 @@ export default {
           id: 1,
           name: 'Zoe Kim',
           online: true,
-          distance: '500m',
+          message: 3,
         },
         {
           image: 'https://randomuser.me/api/portraits/women/2.jpg',
           id: 2,
           name: 'Monica Biluc',
           online: false,
-          distance: '1500m',
+          message: 1,
         },
         {
           image: 'https://randomuser.me/api/portraits/women/3.jpg',
           id: 3,
           name: 'Tatiana Carder',
           online: true,
-          distance: '700m',
+          message: 0,
         },
       ];
     },
