a9f0dba62e06771bb06f7f7bfc761be84818db07
Roman Mahola <roman.mahola@binary-studio.com>
2022-04-01 11:42:34 +0300

    Fix bugs for production

--- frontend/src/bundles/common/pages/LayoutPage.vue
+++ frontend/src/bundles/common/pages/LayoutPage.vue
@@ -8,20 +8,8 @@
 import namespace from '../store/modules/user/namespace';
 import { mapActions } from 'vuex';
 import { FETCH_SESSION_USER } from '../store/modules/user/types/actions';
-import store from '@/store';
 
 export default {
-  beforeRouteEnter(to, from, next) {
-    if (
-      store.state.user.isAuthorized ||
-      to.name.includes('auth') ||
-      to.name === 'home'
-    ) {
-      next();
-    }
-
-    next({ name: 'auth-login' });
-  },
   created() {
     return this.fetchUser();
   },
